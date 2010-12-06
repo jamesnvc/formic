12058aecf0f1bbbc78dad64d73df27876026211d
James N. V. Cash <james.nvc@gmail.com>
2010-12-06 09:37:47 -0500

    Moving to snapshot of master to avoid *agent* bug in alpha3, removing unusued dep on clojure-contrib

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject formic "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.3.0-alpha3"]
-                 [org.clojure.contrib/complete "1.3.0-alpha3"]
-                 ])
+  :dependencies [[org.clojure/clojure "1.3.0-alpha3-SNAPSHOT"]])
 
