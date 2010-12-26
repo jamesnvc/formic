31af33ad65c82adbe81e266ce2f1ff17127983b6
James N. V. Cash <james.nvc@gmail.com>
2010-12-26 12:26:28 -0500

    Improving proof of concept js execution code to allow passing in functions as well as conversion of return values to clojure datastructures

--- src/formic/poc/script_loading.clj
+++ src/formic/poc/script_loading.clj
@@ -1,25 +1,43 @@
 (ns formic.poc.script-loading
-  (:require
-    [formic.poc.agents-board :as board])
   (:import (org.mozilla.javascript Context ScriptableObject
-                                   NativeObject)))
+                                   NativeObject Function)))
 
-(defn object-to-map
+(defn isNativeObject?
+  [obj]
+  (= (class obj) 'org.mozilla.javascript.NativeObject))
+
+
+(defmulti process-js-value class)
+
+(defmethod process-js-value org.mozilla.javascript.NativeObject
   [#^NativeObject obj]
   (into {}
-        (map #(vector (keyword %) (.get obj % nil)) (.getIds obj))))
+        (map #(vector (keyword %) (process-js-value (.get obj % nil)))
+             (.getIds obj))))
+
+(defmethod process-js-value :default
+  [obj]
+  obj)
 
 (defn execute-code
   "Takes javascript code as a string and calls it by evaluating the
   performAction() function with no arguments."
-  [code]
-  (let [cx (Context/enter)
-        scope (.initStandardObjects cx)
-        input (Context/javaToJS code scope)
-        script (str code "performAction();")]
-    (try
-      (ScriptableObject/putProperty scope "input" input)
-      (let [result (.evaluateString cx scope script
-                                    "<cmd>" 1 nil)]
-        result)
-      (finally (Context/exit)))))
+  ([^String code]
+   (execute-code code []))
+  ([^String code add-to-scope]
+    (let [cx (Context/enter)
+          scope (.initStandardObjects cx nil true)
+          input (Context/javaToJS code scope)
+          script (str code "performAction();")]
+      (try
+        (ScriptableObject/putProperty scope "input" input)
+        (doall
+          (for [[prop val] add-to-scope]
+            (let [func (proxy [Function] []
+                         (call [cx scope thisObj args]
+                           (apply val args)))]
+              (ScriptableObject/putProperty scope prop
+                                          (Context/javaToJS func scope)))))
+        (let [result (.evaluateString cx scope script "<cmd>" 1 nil)]
+          (process-js-value result))
+      (finally (Context/exit))))))
